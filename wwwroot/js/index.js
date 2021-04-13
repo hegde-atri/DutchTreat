6253891d7f2f33eddd0f678267c62c84d3988a84
hegde-atri <iamatrihegde@gmail.com>
2021-04-13 18:52:04 +0100

    jQuery and more styles

---
+++ wwwroot/js/index.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function() {
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click",
+        function() {
+            console.log("buying item");
+        });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click",
+        function() {
+            console.log("You clicked on " + $(this).text());
+        });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function(){
+        $popupForm.slideToggle(250);
+
+        });
+
+});
